008b21b3474c740165f4368a847d8e2cc0563a3c
newveryallen <newveryallen3@gmail.com>
2016-10-04 16:35:56 +0800

    forth commit
    whatever

--- LabWebServletJdbcSpringMaven01/src/main/java/haha/us.java
+++ LabWebServletJdbcSpringMaven01/src/main/java/haha/us.java
@@ -26,16 +26,14 @@ public class us extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("yAAAAA!!!!");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		this.doGet(request,response);
 	}
 
 }
